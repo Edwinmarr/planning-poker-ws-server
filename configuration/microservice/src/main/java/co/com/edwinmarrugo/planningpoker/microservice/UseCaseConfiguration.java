a549611a1453deb4d54312d325c52b14d2c11132
emarrugo <emarrugo@proteccion.com.co>
2024-12-05 21:12:18 -0500

    initial commit

---
+++ configuration/microservice/src/main/java/co/com/edwinmarrugo/planningpoker/microservice/UseCaseConfiguration.java
@@ -0,0 +1,17 @@
+package co.com.edwinmarrugo.planningpoker.microservice;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Registro de los Casos de uso de la aplicacion.
+ *
+ * Los casos de uso son la parte de la arquitectura limpia donde se define la logica de negocio.
+ *
+ * Los casos de uso deben estar en el modulo domain/usecase
+ *
+ */
+@Configuration
+public class UseCaseConfiguration {
+
+
+}
